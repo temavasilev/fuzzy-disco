38de699b7fad2122a4bc41c32505282fd5d4b845
MinxuanQin <49282887+MinxuanQin@users.noreply.github.com>
2019-05-02 10:16:09 +0200

    Package class

--- Package.m
+++ Package.m
@@ -1,31 +1,34 @@
 classdef Package
-    %UNTITLED2 此处显示有关此类的摘要
-    %   此处显示详细说明
     
     properties (Constant)
-        size = 20;  %随便写的
+        size = 20;  %Size of the package is defined by the results of sampling
+                    %The value is defined before package entered queue.
+                    
     end
     
     properties 
-        delay;
-        priority_value;
+        delay;      %Delay itself is a variable, caculated in the whole process.
+                    %Delay from transmission and waiting time should be written in. 
+                    %What's the relation between delay and priority?
+                    
+        priority_value;     %Still not clear what the priority depends on 
     end
     
     methods
-        function obj = Package(priority)
+        function obj = Package(priority)                %Constructor
             if ( priority >= 0 && priority <= 150 )
             obj.priority_value = priority;
-            obj.delay = 1 / priority;  %随便写的
+            obj.delay = 1 / priority;                   %Delay is inversely proportional to its priority
             else
                 error('Package can not be created with given priority.');
             end
         end
-        
-        function processing_delay = get.delay(obj)
+            
+        function processing_delay = get.delay(obj)      %Get-function for delay
             processing_delay = obj.delay;
         end
         
-        function priority = get.priority_value(obj)
+        function priority = get.priority_value(obj)     %Get-function for priority
             priority = obj.priority_value;
         end
     end
