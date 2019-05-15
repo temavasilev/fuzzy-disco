0f1f71494787149f220a1066b32b9ea6a2f8c53f
Yong Feng <44069963+FengYong07@users.noreply.github.com>
2019-05-16 01:05:09 +0200

    Update Package.m
    Delay has been redefined and limit of the longest delay has been added.

--- Package.m
+++ Package.m
@@ -7,10 +7,10 @@ classdef Package
     end
     
     properties 
-        delay;      %Delay itself is a variable, caculated und accumulated in the whole process.
+        delay;      %Delay itself is a variable, caculated and accumulated in the whole process.
                     %Delay from transmission and waiting time should be written in. 
                     %What's the relation between delay and priority?
-        max_delay;  %The longest accepted delay for every package
+        max_delay;  %The longest accepted delay for every package. (That should be at least longer than transmission delay)
                     
         priority_value;     %Still not clear what the priority depends on 
     end
