9d9034d8ddfc31c4ab1509088814fd8c294b2508
palomena <palomena@protonmail.ch>
2019-06-02 14:54:09 +0200

    Updated History and Record to use cycles

--- History.m
+++ History.m
@@ -8,55 +8,66 @@ classdef History < handle
         s_actual = Record.empty % Actual signal
         weight % The weight of the actual signal in adjustments
         size % The size of both s_predicted and s_actual
-        tmax % The maximum time; if exceeded, we reset
         flag % Indicates wether s_predicted is holding data
-        dist % The step size in TU
     end
     
     methods
-        function obj = History(tmax,step_size)
+        function obj = History(size)
             %History Construct an instance of this class
             %   Creates suitable Records
-            %   tmax: maximum time in Time Units TU
             %   step_size: interval between recordings in time units TU
-            obj.tmax = tmax;
-            obj.size = ceil(tmax/step_size);
-            obj.s_predicted = Record(obj.size,step_size);
-            obj.s_actual = Record(obj.size,step_size);
+            obj.size = size;
+            obj.s_predicted = Record(obj.size);
+            obj.s_actual = Record(obj.size);
             obj.weight = 1;
-            obj.flag = 0;
-            obj.dist = step_size;
+            obj.flag = -1;
         end
         
         function slope = predict(obj,t)
-            %predict Predicts the estimated slope for the next time window
+            %predict Predicts the next value for the next cycle
             %   Detailed explanation goes here
-            slope = obj.s_predicted.at(t+obj.dist) - obj.s_predicted.at(t);
+            % slope = obj.s_predicted.at(t+obj.dist) - obj.s_predicted.at(t);
+            if(t+1 <= obj.size)
+                slope = obj.s_predicted.at(t+1);
+            else
+                slope = 0;
+            end
         end
         
         function record(obj,t,amplitude)
             %record Records an actual signal value
             %   Detailed explanation goes here
-            obj.s_actual.set(t,amplitude);
+            if(t <= obj.size)
+                obj.s_actual.set(t,amplitude);
+            end
         end
         
         function adjust(obj)
            %adjust Adjusts the predicted signal given a full actual signal
            %    Adjustment is done according to the following formula:
-           %    p = p + (p-a)/(c+1)
+           %%%%% previously:    p = p + (p-a)/2
+           %    p = (p + a)/2
            %    p: predicted signal
            %    a: actual signal
            %    c: weight
            
-           for i=1:obj.size
-               diff = (obj.s_predicted.at(i) - obj.s_actual.at(i)) * obj.flag;
-               amplitude = obj.s_predicted.at(i) + diff / obj.weight;
-               obj.s_predicted.set(i,amplitude);
+           % If we do not have a prediction yet, we just copy the actual
+           % signal to the prediction
+           if(obj.flag == -1)
+               for i=1:obj.size
+                  obj.s_predicted.set(i, obj.s_actual.at(i));
+               end
+               
+              obj.flag = 1; % Once we set s_predicted, we can set the flag to 1
+              return
            end
            
-           obj.flag = 1; % Once we set s_predicted, we can set the flag to 1
-           obj.weight = obj.weight + 1;
-        
+           for i=1:obj.size
+               %diff = (obj.s_predicted.at(i) - obj.s_actual.at(i))/2;
+               %amplitude = obj.s_predicted.at(i) + diff;
+               amplitude = (obj.s_predicted.at(i) + obj.s_actual.at(i))/2;
+               obj.s_predicted.set(i,amplitude);
+           end
         end
     end
 end
--- Record.m
+++ Record.m
@@ -6,29 +6,25 @@ classdef Record < handle
     properties (SetAccess = private)
         field % one-dimensional array storing discrete equidistant signal data
         size % The actual array length of Record.field
-        dist % The time step size / interval
     end
     
     methods
-        function obj = Record(size,dist)
+        function obj = Record(size)
             %Record Construct an instance of this class
             %   Initializes class properties with default values
             %   size: The actual array length, NOT THE MAXIMUM TIME!
-            %   dist: The time step size / interval
             obj.field = zeros(1,size);
             obj.size = size;
-            obj.dist = dist;
         end
         
         function amplitude = at(obj,t)
             %at Returns the recorded amplitude at the given time t
-            %   Converts the time value t to an array index and
             %   returns the corresponding array field.
             %   Use like this:
             %   record.at(10);
             %   to return the record amplitude at time 10 TU where
             %   TU : Time unit
-            index = ceil(t / obj.dist);
+            index = t;
             if(index > obj.size)
                error('index exceeds record size!'); 
             end
@@ -37,14 +33,13 @@ classdef Record < handle
         
         function set(obj,t,amplitude)
             %set Records an amplitude at the given time t
-            %   Converts the time value t to an array index and
             %   sets the corresponding array field equal to amplitude.
             %   Use like this:
             %   record.set(10, 5);
             %   to set the record amplitude at time 10 TU to 5 AU where
             %   TU : Time unit
             %   AU : Amplitude unit
-            index = ceil(t / obj.dist);
+            index = t;
             if(index > obj.size)
                error('Index exceeds record size!'); 
             end
